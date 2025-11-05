e934265528373ca3d9e6a4e64f5ca3ff6d8f2a01
Sampath Perera <22ug3-0620@sltc.ac.lk>
2025-11-05 23:08:51 +0530

    Re-arranged the codes

--- MergeSort.java
+++ MergeSort.java
@@ -52,7 +52,6 @@ public class MergeSort {
         }
         while (i < L.size()) arr.set(k++, L.get(i++));
         while (j < R.size()) arr.set(k++, R.get(j++));
-         while (i < L.size()) arr.set(k++, L.get(i++));
-        while (j < R.size()) arr.set(k++, R.get(j++));
+        
     }
 }
